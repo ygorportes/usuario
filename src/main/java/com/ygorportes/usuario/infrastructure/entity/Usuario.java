174bd625489b7ea88a31e3e388c0ece95c6d9502
Ygor Portes <ygorsportes@gmail.com>
2025-09-14 20:47:30 -0300

    feat: adicionadas classes de security

---
+++ src/main/java/com/ygorportes/usuario/infrastructure/entity/Usuario.java
@@ -0,0 +1,57 @@
+package com.ygorportes.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 100)
+    private String nome;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usario_id", referencedColumnName = "id")
+    private List<Endereco> enderecos;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usario_id", referencedColumnName = "id")
+    private List<Telefone> telefones;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
---
+++ src/main/java/com/ygorportes/usuario/infrastructure/security/JwtUtil.java
@@ -0,0 +1,58 @@
+package com.ygorportes.usuario.infrastructure.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtUtil {
+
+    // Chave secreta usada para assinar e verificar tokens JWT
+    private final String secretKey = "chave-secreta";
+
+
+
+    // Gera um token JWT com o nome de usuário e validade de 1 hora
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username) // Define o nome de usuário como o assunto do token
+                .setIssuedAt(new Date()) // Define a data e hora de emissão do token
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // Define a data e hora de expiração (1 hora a partir da emissão)
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256) // Converte a chave secreta em bytes e assina o token com ela
+                .compact(); // Constrói o token JWT
+    }
+
+    // Extrai as claims do token JWT (informações adicionais do token)
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8))) // Define a chave secreta para validar a assinatura do token
+                .build()
+                .parseClaimsJws(token) // Analisa o token JWT e obtém as claims
+                .getBody(); // Retorna o corpo das claims
+    }
+
+    // Extrai o nome de usuário do token JWT
+    public String extractUsername(String token) {
+        // Obtém o assunto (nome de usuário) das claims do token
+        return extractClaims(token).getSubject();
+    }
+
+    // Verifica se o token JWT está expirado
+    public boolean isTokenExpired(String token) {
+        // Compara a data de expiração do token com a data atual
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    // Valida o token JWT verificando o nome de usuário e se o token não está expirado
+    public boolean validateToken(String token, String username) {
+        // Extrai o nome de usuário do token
+        final String extractedUsername = extractUsername(token);
+        // Verifica se o nome de usuário do token corresponde ao fornecido e se o token não está expirado
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+}
